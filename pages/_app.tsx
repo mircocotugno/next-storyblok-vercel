9cda4855ba19f43f26dfefef6ad64918e6e0c824
Mirco <mirco.cotugno@gmail.com>
2025-10-30 13:53:21 +0100

    fix import errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { initStoryblok } from "@/lib/sbInit";
+import { HeroUIProvider } from "@heroui/react";
 
 initStoryblok();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <HeroUIProvider>
+      <Component {...pageProps} />;
+    </HeroUIProvider>
+  );
 }
